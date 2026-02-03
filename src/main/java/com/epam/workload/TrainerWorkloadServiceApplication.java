a0f43ee8364a2eb95508902f1e05f9170ad5de33
Abay Kulamkadyr <kulamkadyr.abay@gmail.com>
2026-02-03 14:38:57 +0500

    Added dependencies and code formatter

--- src/main/java/com/epam/workload/TrainerWorkloadServiceApplication.java
+++ src/main/java/com/epam/workload/TrainerWorkloadServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrainerWorkloadServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrainerWorkloadServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrainerWorkloadServiceApplication.class, args);
+    }
 
 }
